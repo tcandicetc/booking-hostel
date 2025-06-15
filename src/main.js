2369bd705a931a6db2e69244c326ef77fad5827d
tcandicetc <candice02110211@gmail.com>
2025-06-15 15:49:58 +0800

    redefine app layout and use vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import { router } from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import './assets/css/style.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
